4dd16a66fb56c64d12009c3ec3e49a03587e7d35
Igor <alterg@niuitmo.ru>
2017-06-19 02:55:46 +0300

    Initial commit

---
+++ src/main/java/protocol/Packet.java
@@ -0,0 +1,13 @@
+package java.protocol;
+
+/**
+ * Created by alterG on 19.06.2017.
+ */
+public class Packet {
+    String message;
+
+    enum TYPE {
+
+    }
+
+}
---
+++ src/main/java/ui/ChatFrame.java
@@ -0,0 +1,10 @@
+package java.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by alterG on 19.06.2017.
+ */
+public class ChatFrame extends JPanel {
+
+}
