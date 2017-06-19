7ad04d7f5d299b2220d1ef55c143b15b20d2a71d
Igor <alterg@niuitmo.ru>
2017-06-19 03:42:23 +0300

    Chat Frame
    1st version of chat UI

--- src/main/java/ui/ChatFrame.java
+++ src/main/java/ui/ChatFrame.java
@@ -1,10 +1,51 @@
-package java.ui;
+package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
+ * Frame contains only ChatPanel
  * Created by alterG on 19.06.2017.
  */
-public class ChatFrame extends JPanel {
+public class ChatFrame extends JFrame {
+
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 500;
+    public static final String SEND_BUTTON_NAME = "Send";
+
+    public ChatFrame() {
+        setTitle("Chat v1.0");
+        setSize(new Dimension(WIDTH, HEIGHT));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(null);
+        addComponents();
+
+        setVisible(true);
+
+    }
+
+    private void addComponents() {
+
+        // add chat area
+        JTextArea chatArea = new JTextArea();
+        chatArea.setBounds(20, 20, 300, 300);
+        this.add(chatArea);
+
+        // add user input area
+        JTextArea userInputArea = new JTextArea();
+        userInputArea.setBounds(20, 360, 300, 60);
+        this.add(userInputArea);
+
+        // add users list label
+        JLabel usersListLabel = new JLabel("Users list:");
+        usersListLabel.setBounds(360, 20, 320, 300);
+        usersListLabel.setVerticalAlignment(JLabel.TOP);
+        this.add(usersListLabel);
+
+        // add sent button
+        JButton sendButton = new JButton(SEND_BUTTON_NAME);
+        sendButton.setBounds(360, 360, 80, 30);
+        this.add(sendButton);
+    }
 
 }
